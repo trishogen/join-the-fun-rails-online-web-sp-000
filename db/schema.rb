a93850688f4c43373fd7c7aa62d0f6b16e31c556
Trish Ogen <patriciaogen@gmail.com>
2019-11-04 23:50:06 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140212205242) do
+ActiveRecord::Schema.define(version: 20191104234746) do
+
+  create_table "passengers", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "rides", force: :cascade do |t|
     t.integer  "passenger_id"
     t.integer  "taxi_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.         "passenger"
+    t.         "taxi"
   end
 
   create_table "taxis", force: :cascade do |t|
